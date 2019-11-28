436becd0e0ab746c0ab09f7c09a6f89efa4d1da1
Timothy Metcalfe <tmetcalfe89@gmail.com>
2019-11-28 07:34:24 -0800

    Updated version number to 1.2
    Ima remember to do this stuff before publishing versions. Believe it.

--- src/main/java/us/timinc/jsonifycraft/JsonifyCraft.java
+++ src/main/java/us/timinc/jsonifycraft/JsonifyCraft.java
@@ -17,7 +17,7 @@ import us.timinc.mcutil.*;
 public class JsonifyCraft {
 	public static final String MODID = "jsonifycraft";
 	public static final String NAME = "JsonifyCraft";
-	public static final String VERSION = "1.1.0";
+	public static final String VERSION = "1.2.0";
 
 	public static Logger LOGGER;
 	public static DescriptionLoader GAME_OBJECTS;
