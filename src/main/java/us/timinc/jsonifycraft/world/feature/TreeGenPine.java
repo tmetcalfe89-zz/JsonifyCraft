47da5bf7e57ea6f61ca9d037d1288be1e953969b
Timothy Metcalfe <tmetcalfe89@gmail.com>
2019-12-10 14:56:46 -0800

    Clarified pine tree gen.
    Cleaned up the verbiage, and moved some things around.

--- src/main/java/us/timinc/jsonifycraft/world/feature/TreeGenPine.java
+++ src/main/java/us/timinc/jsonifycraft/world/feature/TreeGenPine.java
@@ -21,29 +21,34 @@ public class TreeGenPine extends TreeGenAbstract {
 
 	@Override
 	public boolean isSuitableLocation(World world, BlockPos pos, int treeHeight) {
-		for (int i1 = pos.getY(); i1 <= pos.getY() + 1 + treeHeight; ++i1) {
-			int j1;
+		if (!((pos.getY() >= 1) && ((pos.getY() + treeHeight + 1) <= world.getHeight())))
+			return false;
 
-			if (i1 - pos.getY() < j) {
-				j1 = 0;
-			} else {
-				j1 = l;
-			}
+		BlockPos down = pos.down();
+		IBlockState state = world.getBlockState(down);
 
-			BlockPos.MutableBlockPos blockpos$mutableblockpos = new BlockPos.MutableBlockPos();
+		if (!(state.getBlock().canSustainPlant(state, world, down, net.minecraft.util.EnumFacing.UP,
+				(net.minecraft.block.BlockSapling) Blocks.SAPLING)
+				&& (pos.getY() < (world.getHeight() - treeHeight - 1))))
+			return false;
 
-			for (int k1 = pos.getX() - j1; k1 <= pos.getX() + j1; ++k1) {
-				for (int l1 = pos.getZ() - j1; l1 <= pos.getZ() + j1; ++l1) {
-					if (i1 >= 0 && i1 < world.getHeight()) {
-						IBlockState state = world.getBlockState(blockpos$mutableblockpos.setPos(k1, i1, l1));
+		for (int yi = pos.getY(); yi <= (pos.getY() + 1 + treeHeight); ++yi) {
+			int leafSpan = l;
 
-						if (!state.getBlock().isAir(state, world, blockpos$mutableblockpos.setPos(k1, i1, l1)) && !state
-								.getBlock().isLeaves(state, world, blockpos$mutableblockpos.setPos(k1, i1, l1))) {
+			if ((yi - pos.getY()) < j)
+				leafSpan = 0;
+
+			for (int xi = pos.getX() - leafSpan; xi <= (pos.getX() + leafSpan); ++xi) {
+				for (int zi = pos.getZ() - leafSpan; zi <= (pos.getZ() + leafSpan); ++zi) {
+					if ((yi >= 0) && (yi < world.getHeight())) {
+						BlockPos targetPos = new BlockPos(xi, yi, zi);
+						state = world.getBlockState(targetPos);
+
+						if (!state.getBlock().isAir(state, world, targetPos)
+								&& !state.getBlock().isLeaves(state, world, targetPos))
 							return false;
-						}
-					} else {
+					} else
 						return false;
-					}
 				}
 			}
 		}
@@ -56,21 +61,21 @@ public class TreeGenPine extends TreeGenAbstract {
 		int j3 = 1;
 		int k3 = 0;
 
-		for (int l3 = 0; l3 <= k; ++l3) {
-			int j4 = pos.getY() + treeHeight - l3;
+		for (int yi = 0; yi <= k; ++yi) {
+			int yiInverted = (pos.getY() + treeHeight) - yi;
 
-			for (int i2 = pos.getX() - i3; i2 <= pos.getX() + i3; ++i2) {
-				int j2 = i2 - pos.getX();
+			for (int xi = pos.getX() - i3; xi <= (pos.getX() + i3); ++xi) {
+				int distanceFromX = xi - pos.getX();
 
-				for (int k2 = pos.getZ() - i3; k2 <= pos.getZ() + i3; ++k2) {
-					int l2 = k2 - pos.getZ();
+				for (int zi = pos.getZ() - i3; zi <= (pos.getZ() + i3); ++zi) {
+					int distanceFromZ = zi - pos.getZ();
 
-					if (Math.abs(j2) != i3 || Math.abs(l2) != i3 || i3 <= 0) {
-						BlockPos blockpos = new BlockPos(i2, j4, k2);
+					if ((Math.abs(distanceFromX) != i3) || (Math.abs(distanceFromZ) != i3) || (i3 <= 0)) {
+						BlockPos blockpos = new BlockPos(xi, yiInverted, zi);
 						IBlockState state = world.getBlockState(blockpos);
 
 						if (state.getBlock().canBeReplacedByLeaves(state, world, blockpos)) {
-							this.setBlockAndNotifyAdequately(world, blockpos,
+							setBlockAndNotifyAdequately(world, blockpos,
 									PlaintextId.getBlockStateFrom(description.leaf));
 						}
 					}
@@ -93,14 +98,14 @@ public class TreeGenPine extends TreeGenAbstract {
 
 	@Override
 	public void generateLogs(World world, BlockPos pos, int treeHeight, Random rand) {
-		int i4 = rand.nextInt(3);
+		int logStartOffset = rand.nextInt(description.maxHeight - description.minHeight);
 
-		for (int k4 = 0; k4 < treeHeight - i4; ++k4) {
-			BlockPos upN = pos.up(k4);
+		for (int hi = 0; hi < (treeHeight - logStartOffset); ++hi) {
+			BlockPos upN = pos.up(hi);
 			IBlockState state = world.getBlockState(upN);
 
 			if (state.getBlock().isAir(state, world, upN) || state.getBlock().isLeaves(state, world, upN)) {
-				this.setBlockAndNotifyAdequately(world, pos.up(k4), PlaintextId.getBlockStateFrom(description.log));
+				setBlockAndNotifyAdequately(world, pos.up(hi), PlaintextId.getBlockStateFrom(description.log));
 			}
 		}
 	}
@@ -112,30 +117,13 @@ public class TreeGenPine extends TreeGenAbstract {
 		l = 2 + rand.nextInt(2);
 		k = treeHeight - j;
 
-		if (pos.getY() >= 1 && pos.getY() + treeHeight + 1 <= world.getHeight()) {
-			if (!this.isSuitableLocation(world, pos, treeHeight)) {
-				return false;
-			} else {
-				BlockPos down = pos.down();
-				IBlockState state = world.getBlockState(down);
-
-				if (state.getBlock().canSustainPlant(state, world, down, net.minecraft.util.EnumFacing.UP,
-						(net.minecraft.block.BlockSapling) Blocks.SAPLING)
-						&& pos.getY() < world.getHeight() - treeHeight - 1) {
-					state.getBlock().onPlantGrow(state, world, down, pos);
-
-					generateLeaves(world, pos, treeHeight, rand);
-
-					generateLogs(world, pos, treeHeight, rand);
-
-					return true;
-				} else {
-					return false;
-				}
-			}
-		} else {
+		if (!isSuitableLocation(world, pos, treeHeight))
 			return false;
-		}
+
+		generateLeaves(world, pos, treeHeight, rand);
+		generateLogs(world, pos, treeHeight, rand);
+
+		return true;
 	}
 
 }
